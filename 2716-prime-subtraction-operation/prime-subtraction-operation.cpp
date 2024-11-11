3e84f9e7b6f07c873be4cc8e5383621e304bb465
AMANJEE <amangs896@gmail.com>
2024-11-11 22:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 26.8 MB (87.22%) - LeetSync

---
+++ 2716-prime-subtraction-operation/prime-subtraction-operation.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    bool isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        if (n <= 3)
+            return true;
+
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for (int i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+
+    int findPrime(int num, int prev) {
+        int p = num - prev - 1;
+
+        while (num - p > prev && p > 0) {
+            if (isPrime(p)) {
+                return p;
+            } else
+                p--;
+        }
+        return 0;
+    }
+
+    bool primeSubOperation(vector<int>& nums) {
+        int n = nums.size();
+        nums[0] -= findPrime(nums[0], 0);
+        bool fl = true;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i - 1] >= nums[i]) {
+                fl = false;
+                break;
+            }
+        }
+
+        if (fl)
+            return true;
+
+        for (int i = 1; i < n; i++) {
+            nums[i] -= findPrime(nums[i], nums[i - 1]);
+            if (nums[i] <= nums[i - 1])
+                return false;
+        }
+
+        return true;
+    }
+};
